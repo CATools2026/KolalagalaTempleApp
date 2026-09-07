9983ea9410ecd1d409d8326f41afc57e7bbc0a7e
CA Tools_By Chulan Adasuriya <catools2026@gmail.com>
2026-09-07 21:33:54 +0530

    Add WebView settings compatibility

---
+++ app/src/main/java/com/catools/templeapp/ui/TempleSettingsWebCompat.kt
@@ -0,0 +1,11 @@
+package com.catools.templeapp.ui
+
+import com.catools.templeapp.data.TempleSettings
+
+var TempleSettings.javaScriptEnabled: Boolean
+    get() = true
+    set(@Suppress("UNUSED_PARAMETER") value) {}
+
+var TempleSettings.domStorageEnabled: Boolean
+    get() = true
+    set(@Suppress("UNUSED_PARAMETER") value) {}
